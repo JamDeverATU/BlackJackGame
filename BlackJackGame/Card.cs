a95c6d8d11585a7cc2cbeda13e41a2d339b85ac5
JamDeverATU <123546822+JamDeverATU@users.noreply.github.com>
2023-10-20 16:23:36 +0100

    cards-completed/comments

--- BlackJackGame/Card.cs
+++ BlackJackGame/Card.cs
@@ -20,24 +20,27 @@ namespace BlackJackGame
 {
     internal class Card
     {
+        // These properties define the rank and suit of the card.
         public CardRank Rank { get; set; }
         public CardSuit Suit { get; set; }
 
-       public int value
+        // This property calculates and returns the value of the card.
+        public int Value
         {
             get
             {
                 if (Rank == CardRank.Ace)
-                    return 11;
-                if (Rank >= CardRank.Ten) 
-                    return 10;
-                return (int)Rank+2;
+                    return 11; // Ace can be worth 11 points.
+                if (Rank >= CardRank.Ten)
+                    return 10; // Face cards (King, Queen, Jack) are worth 10 points.
+                return (int)Rank + 2; // Other cards have values based on their rank.
             }
         }
+
+        // This method provides a string representation of the card.
         public override string ToString()
         {
-            return $"{Rank} of {Suit}";
+            return $"{Rank} of {Suit}"; // Returns a string like "King of Hearts".
         }
-
     }
 }
