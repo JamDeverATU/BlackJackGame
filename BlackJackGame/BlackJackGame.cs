1b87937aa738170c0088930365b5dceb7e2e030f
JamDeverATU <123546822+JamDeverATU@users.noreply.github.com>
2023-10-20 21:33:12 +0100

    blackjack_game_class

---
+++ BlackJackGame/BlackJackGame.cs
@@ -0,0 +1,17 @@
+﻿using BlackJackGame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJackGame
+{
+  
+    internal class BlackJackGame
+    {
+        
+
+
+    }
+}
