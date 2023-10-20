1aed6a6367ca19b03ee5cc64a1b73564844cee04
JamDeverATU <123546822+JamDeverATU@users.noreply.github.com>
2023-10-20 16:15:33 +0100

    cards1

---
+++ BlackJackGame/Card.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Define an enumeration for card ranks, representing the values of playing cards.
+public enum CardRank
+{
+    Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace
+}
+
+// Define an enumeration for card suits, representing the different suits of playing cards.
+public enum CardSuit
+{
+    Hearts, Diamonds, Clubs, Spades
+}
+
+namespace BlackJackGame
+{
+    internal class Card
+    {
+
+
+
+
+
+    }
+}
