0655eb39eec5e4f829dae1b96539ca25d6a7c44a
JamDeverATU <123546822+JamDeverATU@users.noreply.github.com>
2023-10-23 09:46:27 +0100

    deck

--- BlackJackGame/BlackJackGame.cs
+++ BlackJackGame/BlackJackGame.cs
@@ -7,11 +7,38 @@ using System.Threading.Tasks;
 
 namespace BlackJackGame
 {
-  
+
     internal class BlackJackGame
     {
-        
+        public class BlackjackGame
+        {
+            private List<Card> deck;
+            private List<Card> playerHand;
+            private List<Card> dealerHand;
 
+            public int PlayerChips { get; set; }
+            public int Bet { get; set; }
 
+            public BlackjackGame()
+            {
+                InitializeDeck();
+                playerHand = new List<Card>();
+                dealerHand = new List<Card>();
+            }
+
+            public void InitializeDeck()
+            {
+                deck = new List<Card>();
+                foreach (CardSuit suit in Enum.GetValues(typeof(CardSuit)))
+                {
+                    foreach (CardRank rank in Enum.GetValues(typeof(CardRank)))
+                    {
+                        deck.Add(new Card { Suit = suit, Rank = rank });
+                    }
+                }
+            }
+
+        }
     }
+
 }
